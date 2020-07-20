2e287e15e2cca651412a228987ac526124ca28c1
mahme <mustafasaleh.hf@gmail.com>
2020-07-20 19:25:01 +0200

    modify redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,7 +12,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (a) => a
   )
 );
 
