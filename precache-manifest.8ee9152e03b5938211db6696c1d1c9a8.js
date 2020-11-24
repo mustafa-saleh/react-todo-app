ec6ac3fd2f7e4f5fe28b47cc294147c9b6dcce51
mahme <mustafasaleh.hf@gmail.com>
2020-11-24 12:47:24 +0200

    Updates

---
+++ precache-manifest.8ee9152e03b5938211db6696c1d1c9a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a985071b7a8e95bb9db0273ef5a4b4c",
+    "url": "/personal_assistant/index.html"
+  },
+  {
+    "revision": "b6faacd286c7b2e55c13",
+    "url": "/personal_assistant/static/css/main.1bb681b4.chunk.css"
+  },
+  {
+    "revision": "02064c9243ba0a807c52",
+    "url": "/personal_assistant/static/js/2.f462dee4.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/personal_assistant/static/js/2.f462dee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6faacd286c7b2e55c13",
+    "url": "/personal_assistant/static/js/main.8d54fad3.chunk.js"
+  },
+  {
+    "revision": "d01617f32fa810c0d0fb",
+    "url": "/personal_assistant/static/js/runtime-main.27c0b029.js"
+  }
+]);
